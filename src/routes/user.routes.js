1c83ab8ca8272afc9bf1d1978b4d92a2b33e4fd0
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-02-12 21:01:49 +0530

    routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,24 @@
-const express=require("express")
-const router=express.router
-const {handleUserCollection}=require("../controller/user.js")
-//create four router for users
+const express = require("express");
+const router = express.Router();
 
-router.post('/create',handleUserCollection)
+const { handleUserCollection } = require("../controller/user.js");
+
+// Create user
+router.post("/create", handleUserCollection);
+
+// Get all users
+router.get("/", (req, res) => {
+  res.send("Get all users");
+});
+
+// Update user
+router.put("/update/:id", (req, res) => {
+  res.send(`Update user with id ${req.params.id}`);
+});
+
+// Delete user
+router.delete("/delete/:id", (req, res) => {
+  res.send(`Delete user with id ${req.params.id}`);
+});
+
+module.exports = router;
