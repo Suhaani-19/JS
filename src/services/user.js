77df2388b1b1754c54b1906ed1dc5c453a161776
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-02-28 20:59:02 +0530

    changes

--- src/services/user.js
+++ src/services/user.js
@@ -14,7 +14,6 @@ async function createUser(userData) {
             data: userData
         });
 
-        // Return the created user data
         return data;
     } catch (error) {
         console.error("Error creating user:", error);
