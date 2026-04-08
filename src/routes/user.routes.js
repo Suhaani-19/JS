bcf89edde04dc736f8b22da7218ebe5e3ce413a2
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-04-08 09:15:37 +0530

    comments

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+// improved
 router.use(express.json());   
 
 const { handleUserCollection } = require("../controller/user.js");
