5992a80e2d4e6341827fcc944ad265570e8f00a5
Suhaani Garg <suhaanigarg2311@gmail.com>
2026-05-26 20:59:19 +0530

    even

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,7 @@ router.post("/create", handleUserCollection);
 router.get("/", (req, res) => {
   res.send("Get all users");
 });
+
 router.put("/update/:id", (req, res) => {
   res.send(`Update user with id ${req.params.id}`);
 });
